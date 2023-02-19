4b726f0c31cb54047cd22b1ba7fa3bfbbf5e7eba
Kateryna Artemieva <moruga.katya@gmail.com>
2023-02-19 09:19:59 +0000

    task_2

--- js/script.js
+++ js/script.js
@@ -1,16 +1,34 @@
+// /**
+//  *? Використовуючи функцію if...else,
+//  *? напишіть код, який запитуватиме:
+//  *? "Яка офіційна назва JavaScript?"
+//  *? Якщо користувач вводить "ECMAScript",
+//  *? то показати через alert: "Вірно!"
+//  *? інакше відобразити:"Не знаєте? ECMAScript!"
+//  */
+
+// const userInput = prompt("Яка офіційна назва JavaScript ?");
+
+// if (userInput === "ECMAScript") {
+//   alert("Вірно!");
+// } else {
+//   alert("Не знаєте? ECMAScript!");
+// }
+
 /**
- *? Використовуючи функцію if...else,
- *? напишіть код, який запитуватиме:
- *? "Яка офіційна назва JavaScript?"
- *? Якщо користувач вводить "ECMAScript",
- *? то показати через alert: "Вірно!"
- *? інакше відобразити:"Не знаєте? ECMAScript!"
+ *? Напишіть програму, яка отримає від користувача
+ *? число (кількість хвилин) та виведе в консоль
+ *? рядок у форматі годин та хвилин
+ *? https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
+ *? 70 === 01:10
  */
 
-const userInput = prompt("Яка офіційна назва JavaScript ?");
+const userTime = prompt('Введіть число');
+ function time(number){
+let hours = String(Math.floor(number / 60)).padStart(2, "0");
+let minutes = String(number % 60).padStart(2, "0");
+return `${hours}:${minutes}`;
+ }
 
-if (userInput === "ECMAScript") {
-  alert("Вірно!");
-} else {
-  alert("Не знаєте? ECMAScript!");
-}
+console.log(time(userTime));
+
